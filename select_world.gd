97accfd7c4ced39f1f57a1a28465e1fcb04c39cd
Zedartt <julianpotier13@gmail.com>
2025-11-03 16:54:46 +0100

    add world_&_level_selection

--- select_world.gd
+++ select_world.gd
@@ -1,4 +1,14 @@
 extends Node
 
-func _on_world_1_button_pressed() -> void:
-	pass # Replace with function body.
+@onready var go_world_1 = preload("res://world_1.tscn")
+@onready var go_world_2 = preload("res://world_2.tscn")
+@onready var go_back = preload("res://MainMenu.tscn")
+
+func _on_world_1_pressed() -> void:
+	get_tree().change_scene_to_packed(go_world_1)
+
+func _on_world_2_pressed() -> void:
+	get_tree().change_scene_to_packed(go_world_2)
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
