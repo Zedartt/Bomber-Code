2a3f96e690dd027f4fb3513bef51c790694edb79
Zedartt <julianpotier13@gmail.com>
2025-11-05 19:25:57 +0100

    mise a jour flemme de message

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,5 @@
 extends Control
 @onready var select_code_language = preload("res://Select_world.tscn")
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_leave_buton_pressed() -> void:
 	get_tree().quit()
@@ -8,6 +7,6 @@ func _on_leave_buton_pressed() -> void:
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(select_code_language)
 
-func _process(delta):
-	if Input.is_action_just_pressed("click"):
-		audio_stream_player.play()
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://shop.tscn")
